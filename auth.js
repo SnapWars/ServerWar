1ddde587f9936ac40d345d903c8e3264e1e8d067
David Zhang <davidozhang@gmail.com>
2020-01-20 20:06:32 -0800

    Clean up

--- auth.js
+++ auth.js
@@ -8,4 +8,14 @@ passport.use(snapchatStrategy);
 
 router.use(passport.initialize());
 
+// https://github.com/Snapchat/passport-snapchat/blob/master/README.md
+router.get('/auth/snapchat',
+  passport.authenticate('snapchat', { scope: ['user.display_name', 'user.bitmoji.avatar'] }));
+
+router.get('/auth/snapchat/callback',
+  passport.authenticate('snapchat', { failureRedirect: '/login' }),
+  function(req, res) {
+    console.log('Successful authentication');
+  });
+
 module.exports = router;
--- strategies/snapchat.js
+++ strategies/snapchat.js
@@ -1,9 +1,10 @@
 const SnapchatStrategy = require('passport-snapchat').Strategy;
 
 module.exports = new SnapchatStrategy({
-    clientID: process.env.SNAPCHAT_APP_ID,
-    clientSecret: process.env.SNAPCHAT_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/snapchat/callback"
+    clientID: process.env.SNAPCHAT_CLIENT_ID,
+    clientSecret: process.env.SNAPCHAT_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/snapchat/callback",
+    profileFields: ['id', 'displayName', 'bitmoji']
   },
   async (accessToken, refreshToken, profile, cb) => {
     // TODO: Process tokens & profile
